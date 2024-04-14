9b9512eb2853c7df4081b5385c2c8005605f2c09
nsk-05 <sanjaykumar.nsk5@gmail.com>
2024-04-14 17:09:07 +0530

    map server added

--- d_bot_slam/setup.py
+++ d_bot_slam/setup.py
@@ -13,6 +13,7 @@ setup(
         ('share/' + package_name, ['package.xml']),
         (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py')),
         (os.path.join('share', package_name, 'config'), glob('config/*')),
+        (os.path.join('share', package_name, 'maps'), glob('maps/*')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
