86573f01aead678d0512c0bf1e077370f9f39949
nsk-05 <sanjaykumar.nsk5@gmail.com>
2024-04-17 02:04:20 +0530

    autonomous mapping pkg added

---
+++ d_bot_exploration/launch/explore.launch.py
@@ -0,0 +1,80 @@
+import os
+
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+    d_bot_exploration_dir = get_package_share_directory('d_bot_exploration')
+    d_bot_slam_dir = get_package_share_directory('d_bot_slam')
+    d_bot_navigation_dir = get_package_share_directory('d_bot_navigation')
+    explore_lite_launch = os.path.join(
+        get_package_share_directory('explore_lite'), 'launch', 'explore.launch.py'
+    )
+
+    params_file = LaunchConfiguration('params_file')
+    use_sim_time = LaunchConfiguration('use_sim_time')
+
+    declare_params_file_cmd = DeclareLaunchArgument(
+        'params_file',
+        default_value=os.path.join(d_bot_exploration_dir, 'config', 'explore.yaml'),
+        description='Full path to the ROS2 parameters file to use for all launched nodes',
+    )
+
+    declare_use_sim_time_cmd = DeclareLaunchArgument(
+        'use_sim_time', default_value='false', description='Use simulation (Gazebo) clock if true'
+    )
+
+    slam_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(os.path.join(d_bot_slam_dir, 'launch', 'd_bot_slamtoolbox.launch.py')),
+        launch_arguments={
+            'use_sim_time': use_sim_time,
+            'use_rviz': 'false'
+        }.items(),
+    )
+
+    nav2_bringup_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(os.path.join(d_bot_navigation_dir, 'launch', 'd_bot_navigation.launch.py')),
+        launch_arguments={
+            'use_sim_time': use_sim_time,
+            'params_file': params_file,
+        }.items(),
+    )
+
+    explore_lite_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([explore_lite_launch]),
+        launch_arguments={
+            'use_sim_time': use_sim_time,
+        }.items(),
+    )
+
+    rviz_config_dir = os.path.join(
+        d_bot_exploration_dir,
+        'rviz',
+        'explore.rviz',
+    )
+
+    rviz_node = Node(
+        package='rviz2',
+        executable='rviz2',
+        name='rviz2',
+        arguments=['-d', rviz_config_dir],
+        parameters=[{'use_sim_time': use_sim_time}],
+        output='screen',
+    )
+
+    ld = LaunchDescription()
+
+    ld.add_action(declare_params_file_cmd)
+    ld.add_action(declare_use_sim_time_cmd)
+
+    ld.add_action(slam_launch)
+    ld.add_action(nav2_bringup_launch)
+    ld.add_action(explore_lite_launch)
+    # ld.add_action(rviz_node)
+
+    return ld
--- d_bot_navigation/launch/d_bot_navigation.launch.py
+++ d_bot_navigation/launch/d_bot_navigation.launch.py
@@ -1,5 +1,6 @@
 import os
 from ament_index_python.packages import get_package_share_directory
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
 from launch import LaunchDescription
 from launch_ros.actions import Node
 
@@ -11,44 +12,40 @@ def generate_launch_description():
     recovery_yaml = os.path.join(get_package_share_directory('d_bot_navigation'), 'config', 'recovery.yaml')
 
     
-    return LaunchDescription([     
-        Node(
+    is_explore = DeclareLaunchArgument(
+        'is_explore',
+        default_value=False,
+        description="Indicating whether autonomous mapping or navigation",
+    )
+
+    nav_controller = Node(
             package='nav2_controller',
             executable='controller_server',
             name='controller_server',
             output='screen',
-            parameters=[controller_yaml]),
+            parameters=[controller_yaml])
 
-        Node(
+    nav_planner = Node(
             package='nav2_planner',
             executable='planner_server',
             name='planner_server',
             output='screen',
-            parameters=[planner_yaml]),
+            parameters=[planner_yaml])
             
-        # Node(
-        #     package='behavior_server',
-        #     executable='recoveries_server',
-        #     name='recoveries_server',
-        #     parameters=[recovery_yaml],
-        #     output='screen'),
-
-        Node(
+    behaviour_server = Node(
             package='nav2_behaviors',
             executable='behavior_server',
             name='behavior_server',
             output='screen',
-            parameters=[recovery_yaml],
-        ),
-
-        Node(
+            parameters=[recovery_yaml],)
+    
+    nav_bt_server = Node(
             package='nav2_bt_navigator',
             executable='bt_navigator',
             name='bt_navigator',
             output='screen',
-            parameters=[bt_navigator_yaml]),
-
-        Node(
+            parameters=[bt_navigator_yaml])
+    lifecycle_server = Node(
             package='nav2_lifecycle_manager',
             executable='lifecycle_manager',
             name='lifecycle_manager_pathplanner',
@@ -58,4 +55,12 @@ def generate_launch_description():
                                         'controller_server',
                                         'behavior_server',
                                         'bt_navigator']}])
-    ])
+    
+    ld = LaunchDescription()
+    ld.add_action(nav_controller)
+    ld.add_action(nav_planner)
+    ld.add_action(behaviour_server)
+    ld.add_action(nav_bt_server)
+    ld.add_action(lifecycle_server)
+
+    return ld
